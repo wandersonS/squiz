858f9d1c5f0dae7a5e4dd5c726e16fd27bd8a867
Wanderson da Silva Barbosa <wandersomhto@hotmail.com>
2017-11-22 21:51:05 -0200

    Pagina simples
    só tem os formulario e algumas imagens e alimenta o banco de dados

---
+++ registrar.php
@@ -0,0 +1,11 @@
+﻿<?php
+	include_once('conexao.php');
+	$Nome = $_POST['Nome'];
+	$Sobrenome = $_POST['Sobrenome'];
+	$DataNascimento = $_POST['DataNascimento'];
+	$Sexo = $_POST['Sexo'];
+	$Email = $_POST['Email'];
+	$Senha = $_POST['Senha'];	
+	$inserir=$pdo->prepare("INSERT INTO usuario(email,senha,nome,data_nascimento,sexo,sobrenome) values ('$Email','$Senha','$Nome','$DataNascimento','$Sexo','$Sobrenome')");
+	$inserir -> execute();
+?>
